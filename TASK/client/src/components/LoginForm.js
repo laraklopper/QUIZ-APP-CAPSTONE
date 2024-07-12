bd16486fe554d78fce63ce0bbaf5b51915155678
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-07-12 11:35:53 +0200

    Update LoginForm.js

--- TASK/client/src/components/LoginForm.js
+++ TASK/client/src/components/LoginForm.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import '../CSS/Login.css'
+// Import necessary modules and packages
+import React from 'react';
 //Bootstrap
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,79 +7,77 @@ import Button from 'react-bootstrap/Button';
 
 //LoginForm function component
 export default function LoginForm(
-    {//PROPS PASSED FROM PARENT COMPONENT
-        setShowPassword,
-        showPassword, 
-        userData,
-        handleLoginInput,
-        handleLogin
-    }
-    ) {
+  {//PROPS PASSED FROM PARENT COMPONENT
+    handleLogin, 
+    handleUserLogin, // Event listener to handle user Login
+    userData,
+    setShowPassword,
+    showPassword
+  }
+) {
 
-    //=========JSX RENDERING==============
+
+
+  //============JSX RENDERING================
+
   return (
-    <form id='LoginForm' onSubmit={handleLogin}>
-        <div id='loginDetails'>
-            <Row className='loginRow'>
-                <Col className='loginCol'>
-                {/* Username input */}
-                    <label className='loginLabel' htmlFor='username'>
-                        <p className='labelText'>USERNAME:</p>
-                        <input
-                        className='loginInput'
-                        id='username'
-                        type='text'
-                        name='username'
-                        value={userData.username}
-                        onChange={handleLoginInput}
-                        autoComplete='off'
-                        placeholder='USERNAME'
-                        required
-                        />
-                    </label>
-                </Col>
-            </Row>
-            <Row className='loginRow'>
-                <Col className='loginCol'>
-                {/* Password Input*/}
-                    <label className='loginLabel' htmlFor='password'>
-                        <p className='labelText'>PASSWORD</p>
-                        <input
-                        className='loginInput'
-                        id='password'
-                        type={showPassword ? 'text' : 'password'}
-                        autoComplete='off'
-                        name='password'
-                        value={userData.password}
-                        onChange={handleLoginInput}
-                        placeholder='PASSWORD'   
-                           />
-                        <div id='showPassword'>
-                            {/* Button to display password */}
-                              <Button 
-                              variant="primary"
-                              id='passwordDisplay'
-                              type='button'
-                                  onMouseDown={(e) => { e.preventDefault(); setShowPassword(true); }}
-                                  onMouseUp={(e) => { e.preventDefault(); setShowPassword(false); }}
-                              >
-                                  {showPassword ? 'HIDE PASSWORD' : 'SHOW PASSWORD'}
-                                </Button>
-                        </div>
-                    </label>
-                </Col>
-            </Row>
-              <Row className='loginRow'>
-                  <Col className='loginCol'>
-                      {/* Login Button */}
-                      <Button variant="primary" id='loginBtn' type='submit'>
-                          LOGIN
-                      </Button>
-                  </Col>
-              </Row>
-        </div>
-        
+    // Form for user login
+    <form id='loginForm' onSubmit={handleLogin}>
+      <div id='loginDetails'>
+      <Row className='loginRow'>
+        <Col className='loginCol'>
+          {/* Username input */}
+          <label className='loginLabel'>
+            <p className='labelText'>USERNAME:</p>
+            <input
+              className='loginInput'
+              type='text'
+              name='username'
+              value={userData.username}
+              onChange={handleUserLogin}
+              autoComplete='off'
+              placeholder='USERNAME'
+            />
+          </label>
+        </Col>
+      </Row>
+      <Row className='loginRow'>
+        <Col className='loginCol'>
+          {/* Password Input */}
+          <label className='loginLabel'>
+            <p className='labelText'>PASSWORD:</p>
+            <input
+              className='loginInput'
+              type={showPassword ? 'text' : 'password'}
+              name='password'
+              value={userData.password}
+              onChange={handleUserLogin}
+              autoComplete='current-password'
+              placeholder='PASSWORD'
+            />
+            <div id='showPassword'>   
+             {/* Button to display password */}
+              <Button variant='success'
+                type='button'
+                id='passwordDisplay'
+                onMouseDown={(e) => { e.preventDefault(); setShowPassword(true); }}
+                onMouseUp={(e) => { e.preventDefault(); setShowPassword(false);}}
+                >
+                  {showPassword ? 'HIDE PASSWORD' : 'SHOW PASSWORD'}
+                </Button>
+              </div>
+          </label>
+        </Col>
+      </Row>
+      <Row className='loginRow'>
+        <Col className='loginCol'>
+          {/* Login Button */}
+          <Button variant="primary" id='loginBtn' type='submit'>
+            LOGIN
+          </Button>
+        </Col>  
+      </Row>
+      </div> 
     </form>
-
   )
 }
