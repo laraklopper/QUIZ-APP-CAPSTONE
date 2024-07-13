173be035b39353c7487447e8faa5e90206341a6c
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-07-13 12:16:19 +0200

    Update userSchema.js

--- TASK/server/models/userSchema.js
+++ TASK/server/models/userSchema.js
@@ -1,29 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');// Import the Mongoose library
+// const bcrypt = require("bcryptjs");
 
+// Define the user schema
 let userSchema = mongoose.Schema({
-    username: {
+    username: { // Field for the username
         type: String,
-        required: true,
+        required: [true, 'Username is required'],
         unique: true,
     },
-    email: {
+    email: {// Field for the email
         type: String,
-        required: true,
+        required: [true, 'User email is required'],
         unique: true,
     },
-    dateOfBirth: {
+    dateOfBirth: {//Field for user date of birth
         type: Date,
-        required: true,
+        required: [true, 'date of birth is required'],
     },
-    admin:{
+    admin:{//Optional field for admin status
         type: Boolean,
         required: false,
     },
-    password:{
+    password:{//Field for user password
         type: String,
-        required: true,
+        required: [true, 'Password is required'],
         unique: true
     }
 }, {timestamp: true})
+
+// userSchema.pre("save", async function () {
+//     this.password = await bcrypt.hash(this.password, 12)
+// })
 
 module.exports = mongoose.model('users', userSchema)
