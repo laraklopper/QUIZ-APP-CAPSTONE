3866a2509d96fac0c5b0df0261b4cf5498ed293e
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-07-17 10:34:44 +0200

    Update users.js

--- TASK/server/routes/users.js
+++ TASK/server/routes/users.js
@@ -1,38 +1,19 @@
 // Import necessary modules and packages
-const express = require('express');// Import Express Web framework 
-const router = express.Router();// Create an Express router
-/*
-The express.Router() function is used to create a new router object. 
-This function is used to create a new router object to handle requests. 
-*/
+const express = require('express');
+const router = express.Router();
+// Import the 'jsonwebtoken' library for handling JSON Web Tokens
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
-const User = require('../models/userSchema')
+//Schemas
+const User = require ('../models/userSchema');
+const {authenticateToken} = require('./middleware');
 
-//=========SETUP MIDDLEWARE==========
-router.use(express.json()); // Parse incoming request bodies in JSON format
-/*Built-Middleware function used to parse incoming requests with JSON payloads.
-Returns middleware that only parses JSON and only looks at requests where the
-Content-Type header matches the type option.*/
-router.use(cors()); //Enable Cross-Origin Resource sharing 
+//=======SETUP MIDDLEWARE===========
+router.use(cors());
+router.use(express.json());
 
-// Middleware to verify JWT and extract user info
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) return res.sendStatus(401);
-
-    jwt.verify(token,
-        'secretKey',
-        (err, user) => {
-            if (err) return res.sendStatus(403);
-            req.user = user;
-            next();
-        });
-};
-
-//==========ROUTES===========
+//=============ROUTES=====================
 /*
 |================================================|
 | CRUD OPERATION | HTTP VERB | EXPRESS METHOD    |
@@ -46,79 +27,84 @@ const authenticateToken = (req, res, next) => {
 |DELETE          | DELETE    |  router.delete()  |
 |================|===========|===================|
 */
-
-//=============GET=====================
-//Route to handle a GET request to fetch a single user
-router.get('/userId', authenticateToken, async (req, res) => {
-    console.log('Finding user');// Log a message in the console for debugging purposes
+//------------------GET---------------
+//Route to handle GET requests to fetch a single user
+router.get('/userId',authenticateToken, async (req, res) => {
+    console.log('Finding User');
     try {
-        // Retrieve user details using the authenticated user ID from req.user.userId
         const user = await User.findById(req.user.userId);
 
-    //Conditional rendering to check if the user exists
          if (!user) {
-            console.error('User not found');//Log an error message in the console for debugging purposes
-            return res.status(400).json(// Return a JSON response with a 400 status code and 'User Not Found' message.
+            console.error('User not found');
+            return res.status(400).json(
                 { message: 'User Not Found'}
             )
          }
-        
-        res.status(200).json(user);//If the user is found, sends a JSON response with the user details and a 200 status code (OK).
-        console.log('User details', user);//Log the user details to the console for debugging purposes.
-        
+
+        res.status(200).json(user);
+        console.log('User details', user);
+         
     } 
     catch (error) {
-     console.error('Error fetching Users', error.message);// Log error message in the console for debugging purposes
-        /* If an error occurs during database operation, 
-    return a 500 Internal Server Error response*/
+        console.error('Error fetching Users', error.message);
         res.status(500).json(
             {message: 'Internal Server Error', error: error.message}
         )
     }
 })
 
-//Route to handle a GET request to fetch all users
-router.get('/findUsers', async (req, res) => {
-    console.log('Finding users');//Log a message in the console for debugging purposes
-    try {
-        const {username} = req.query;// Extract the username from query parameters
-        const query = username ? {username} : {};// Create a MongoDB query object based on whether username is provided or not
-        const users = await User.find(query);// Using the User model to find users based on the query
 
-        console.log(users);//Log the fetched users to the console for debugging purposes
-        res.status(200).json(users);// Send a JSON response with the found users and a 200 (OK) status code
-    } catch (error) {
-        console.error('Error fetching users');// Log error message in the console for debugging purposes
-        res.status(500).json({message: 'Internal Server Error'});// Send a 500 (Internal Server Error)status code and an error message in case of an error
+//Route to GET all users
+router.get('/findUsers', async (req, res) => {
+    console.log('Finding Users');
+
+    try {
+        const { username } = req.query;
+        const query = username ? { username } : {};
+        const users = await User.find(query);
+    
+        console.log(users);
+        res.status(200).json(users);
+    } 
+    catch (error) {
+        console.error('Error fetching users', error.message);
+        res.status(500).json({ message: 'Internal server Error' });
     }
 })
 
-//=================POST==========================
+
+//-------------POST-------------------
 //Route to send POST request to login endpoint
 router.post('/login', async (req, res) => {
-    console.log(res.body);// Log the request body containing the username and password
-    console.log('User Login');// Log a message in the console indicating user login for debugging purposes
+    console.log(req.body);
+    console.log('User Login')
 
     try {
-        const {username, password} = req.body;// Extract username and password from the request body
-        const user = await User.findOne({username, password});// Find the user in the database by username and password
-        console.log(user);// Log a message in the console indicating user login for debugging purposes
+        
+        const { username, password } = req.body;
+        const user = await User.findOne({ username, password });
+        console.log(user);
+
+        if (!user) {
+            throw new Error('User not found')
+        }
 
         if (password === user.password) {
             const jwtToken = jwt.sign(
-                {userId: user._id},
-                'secretKey',
-                /*process.env.JWT_SECRET, */
-                {
-                    expiresIn: '12h',
-                    algorithm: 'HS256',
-                }
-            )
-            res.json({'token' : jwtToken})
-        } else {
-            throw new Error('Password Incorrect')
+                { userId: user._id },
+                        'secretKey',
+                        /*process.env.JWT_SECRET,*/
+                        {
+                            expiresIn: '12h',
+                            algorithm: 'HS256',
+                        }
+                    );
+            res.json({ 'token': jwtToken })
+        } 
+        else {
+            throw new Error('Password Incorrect');
         }
-
+     
     } catch (error) {
         console.error('Login Failed: Username or password are incorrect');
         res.status(401).json({ message: 'User not authenticated' })
@@ -129,14 +115,15 @@ router.post('/login', async (req, res) => {
 router.post('/register', async (req, res) => {
     console.log(req.body);
     try {
-        const { username, email, dateOfBirth, password, admin = false } = req.body;
+        const {username, email, dateOfBirth, password, admin = false } = req.body;
+      
         if (!username || !email || !dateOfBirth || !password) {
-            console.error('Username and password are required');
-            return res.status(400).json(
-                { message: 'Username and password are required' }
-            );
+          console.error('Username and password are required');
+          return res.status(400).json(
+            { message: 'Username and password are required' }
+        );
         }
-
+      
         const existingUser = await User.findOne({ username });
 
         if (existingUser) {
@@ -144,6 +131,7 @@ router.post('/register', async (req, res) => {
                 { message: 'Username already exists' }
             );
         };
+
         const newUser = new User({
             username,
             email,
@@ -166,6 +154,7 @@ router.post('/register', async (req, res) => {
 
         res.status(201).json({ token, user: savedUser });
         console.log(savedUser);
+
     } 
     catch (error) {
         console.error('Failed to add User');
@@ -174,3 +163,62 @@ router.post('/register', async (req, res) => {
         )
     }
 })
+
+//-------------PUT-----------------
+router.put('/editAccount/:id', async (req, res) => {
+    console.log('edit Account');
+    try {
+        const {id} = req.params
+        const {username, email} = req.body
+
+        const updateUser = {}
+        if (username) updateUser.username = username
+        if (email) updateUser.email = email;
+
+        const updatedAccount = await User.findByIdAndUpdate(
+            id,
+            updateUser,
+            { new: true }
+        )
+
+        if (!updatedAccount) {
+            return res.status(404).json({ message: 'User not found' })
+        }
+        console.log('Updated User Account:', updatedAccount);
+        res.status(201).json(
+            { message: 'User account successfully updated', updatedAccount }
+        );
+    } 
+    catch (error) {
+        console.error(`Error occured while updating User Account ${error.message}`);
+        return res.status(500).json({ message: 'Internal server error' })
+
+    }
+})
+
+
+//---------------DELETE--------------------
+//Route to send a DELETE request to the /deleteUser endpoint
+router.delete('/deleteUser/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const removedUser = await User.findByIdAndDelete(id);
+        if (!removedUser) {
+            return res.status(404).json({ message: 'User not found' })
+        }
+
+        res.json({
+            message: 'User Successfully deleted',
+            deletedUserId: removedUser._id
+        })
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.error('Error deleting user:', error.message);
+        res.status(500).json({ error: 'Failed to delete User' });
+    }
+})
+
+
+module.exports = router;// Export the router to be used in other parts of the application
