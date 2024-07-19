a3692d10453037e35879bff141f6b92b6dad0ad7
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-07-19 10:39:31 +0200

    Update Page2.js

--- TASK/client/src/pages/Page2.js
+++ TASK/client/src/pages/Page2.js
@@ -1,15 +1,15 @@
 // Import necessary modules and packages
-import React, { useEffect, useState } from 'react';// Import the React module to use React functionalities
-import '../CSS/Page2.css';//Import CSS stylesheet
+import React, { useEffect, useState } from 'react';
+import '../CSS/Page2.css';
 // Bootstrap
-import Row from 'react-bootstrap/Row'; // Import the Row component from react-bootstrap
-import Col from 'react-bootstrap/Col'; // Import the Col component from react-bootstrap
-import Button from 'react-bootstrap/Button'; // Import the Button component from react-bootstrap
-import Dropdown from 'react-bootstrap/Dropdown';//Import the Dropdown component from react-bootstrap
+import Row from 'react-bootstrap/Row'; 
+import Col from 'react-bootstrap/Col'; 
+import Button from 'react-bootstrap/Button'; 
+import Dropdown from 'react-bootstrap/Dropdown';
 // Components
-import Header from '../components/Header';//Import the Header component from '../components/Header'
-import Footer from '../components/Footer';//Import the Footer component from '../components/Footer'
-import Quiz from '../components/Quiz';//Import the Quiz component from '../components/Quiz'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Quiz from '../components/Quiz';
 
 // Page 2 function component
 export default function Page2({//Export default Page2 function component 
@@ -20,34 +20,42 @@ export default function Page2({//Export default Page2 function component
 }) {
   // =========STATE VARIABLES====================
   // Quiz variables
-  const [selectedQuiz, setSelectedQuiz] = useState(null); //State used to store the selected quiz
-  const [currentQuestion, setCurrentQuestion] = useState(0); // State to store the current question
+  const [selectedQuiz, setSelectedQuiz] = useState(null); 
+  const [currentQuestion, setCurrentQuestion] = useState(0); // => change state variable name
   // Score Variables
   const [score, setScore] = useState(0);// State to store the user's score
   // Timer variables
-  const [timer, setTimer] = useState(null); // State to store the current timer
-  const [quizTimer, setQuizTimer] = useState(false); // Boolean to indicate if the timer should be used
-
+  const [timer, setTimer] = useState(null);
+  const [time, setTime] = useState(30)
+  const [quizTimer, setQuizTimer] = useState(false); 
   //============USE EFFECT HOOK==================
- // useEffect to fetch quizzes when the component mounts or when fetchQuizzes changes
+ /* useEffect to fetch quizzes when the component 
+ mounts or when fetchQuizzes changes*/
 useEffect(() => {
-  fetchQuizzes(); // Call the fetchQuizzes function to retrieve the quiz list
-}, [fetchQuizzes]); // Dependency array: runs this effect whenever fetchQuizzes changes
+  fetchQuizzes(); 
+}, [fetchQuizzes]); 
+  // Dependency array: runs this effect whenever fetchQuizzes changes
 
 
   //=======EVENT LISTENERS============
 
   // Function to handle quiz selection
 const handleSelectQuiz = (quiz) => {
-  setSelectedQuiz(quiz);// Set the selected quiz to the quiz chosen by the user.
-  setCurrentQuestion(0);// Reset the current question index to 0, starting from the first question
-  setScore(0); // Reset the user's score to 0 to ensure the users score starts at 0 when a new quiz is selected 
-  setTimer(null); // Clear the timer (if any), setting it to null
+  setSelectedQuiz(quiz);
+  setCurrentQuestion(0);// => change variable name
+  setScore(0);
+  setTimer(null); 
 };
 
 //Function to move to next question
   const handleNextQuestion = () => {
-    setCurrentQuestion(currentQuestion + 1);// Increment the current question index by 1.
+    if(currentQuestion < selectedQuiz.questions.length - 1){//=> change variable name
+      setCurrentQuestion(currentQuestion + 1)
+    }
+    else(
+      setSelectedQuiz(null);
+      setTimer(null)
+    )
   }
 
   // Function to restart the quiz
@@ -58,33 +66,30 @@ const handleSelectQuiz = (quiz) => {
 
   // Start the quiz and initialize a timer if the timer option is selected
   const handleQuizStart = () => {
-    //Conditional rendering to check if the quiz timer is enabled
+    setCurrentQuestion() //=> change variable name 
+    setScore(0)
     if (quizTimer) {
-      setTimer(30); // Set timer to 30 seconds for each question
+      setTimer(30); // Set timer to 30 seconds for each question => change to 10
       const interval = setInterval(() => {
         setTimer((prevTimer) => {
-         
           if (prevTimer === 1) { 
-            // When the timer reaches 1 second, clear the interval and move to the next question
-            clearInterval(interval); // Stop the interval from running
-            handleNextQuestion();// Call the handleNextQuestion function to move to the next question
-            return null;//Reset the timer to idicate its no longer active
+            clearInterval(interval);
+            handleNextQuestion();
+            return null;
           }
-          return prevTimer - 1;//Decrease the timer by one second
+          return prevTimer - 1;
         });
       }, 1000);//Set the interval to 1 second
+      return () => clearInterval(interval);// Clear interval on component unmount
     }
   };
 
   // Function to handle answer selection and update the score if correct
   const handleAnswerClick = (isCorrect) => {
-    //Conditional rendering to check if the question is correct
     if (isCorrect) {
-      // If correct, increment the score by 1
       setScore(score + 1);
     }
-    handleNextQuestion();/*Call handleNextQuestion to move to 
-    next quiz question, regardless of whether the answer was correct or not*/
+    handleNextQuestion();
   };
 
 
@@ -123,12 +128,12 @@ const handleSelectQuiz = (quiz) => {
               <Col xs={6} md={4}>
                 <label id='addTimerLabel'>
                   <p className='labelText'>ADD TIMER:</p>
-                    {/* Checkbox input to toggle the timer feature */}
+                    {/* Checkbox input to enable timer*/}
                   <input
-                    type='checkbox'//Specify the input type as checkbox
-                    checked={quizTimer}// Sets the checkbox state based on quizTimer
-                    onChange={(e) => setQuizTimer(e.target.checked)} // Updates quizTimer state when changed
-                    id='quizTimer' // Unique ID for the checkbox
+                    type='checkbox'
+                    checked={quizTimer}
+                    onChange={(e) => setQuizTimer(e.target.checked)} 
+                    id='quizTimer' 
                   />
                 </label>
               </Col>
