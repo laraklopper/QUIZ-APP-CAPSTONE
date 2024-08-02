e17d572a3ce6a512989c73d2055e015feb62983e
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-08-02 12:26:45 +0200

    Update utilityFunctions.js

--- TASK/client/src/util/utilityFunctions.js
+++ TASK/client/src/util/utilityFunctions.js
@@ -8,6 +8,17 @@
     return new Date(dateString).toLocaleDateString('en-GB', options);
   }
 
+
+  // Fisher-Yates shuffle algorithm to randomize array elements
+  const shuffleArray = (array) => {
+    let shuffledArray = array.slice(); // Create a copy of the array
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1)); // Generate a random index
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]; // Swap elements
+    }
+    return shuffledArray;
+  };
+
 export defualt = {
   dateDisplay
 }
