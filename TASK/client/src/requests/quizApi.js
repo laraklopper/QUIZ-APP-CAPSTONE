dea769a8a7eace483302313cac2d7cad7cd980cd
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-08-08 13:16:00 +0200

    Update quizApi.js

--- TASK/client/src/requests/quizApi.js
+++ TASK/client/src/requests/quizApi.js
@@ -1,6 +1,20 @@
 const QUIZ_URL =  'http://localhost:3001/quiz';
 
+/*
+|============================|
+| CRUD OPERATION | HTTP VERB | 
+|================|===========|
+|CREATE          | POST      | 
+|----------------|-----------|
+|READ            | GET       |  
+|----------------|-----------|
+|UPDATE          | PUT       |
+|----------------|-----------|
+|DELETE          | DELETE    |  
+|================|===========|
+*/
 
+//Function to fetch all Quizzes
 export const fetchQuizzes = async (token) => {
   const response = await fetch(`${API_URL}/quiz/findQuizzes`, {
     method: 'GET',
@@ -18,6 +32,7 @@ export const fetchQuizzes = async (token) => {
   return response.json();
 };
 
+//Function to fetch single quiz
 export const fetchQuiz = async (quizId, token) => {
   const response = await fetch(`${QUIZ_URL}/quizId/${quizId}`, {
     method: 'GET',
